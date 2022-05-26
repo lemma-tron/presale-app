85b8771acbcc2e78c6aa77a1477d9e156805591d
Umesh <umes4ever@gmail.com>
2022-05-26 23:10:39 +0545

    Decimal busd fixes, Metadata fixes

--- src/libs/callHelpers.js
+++ src/libs/callHelpers.js
@@ -1,10 +1,15 @@
+import BigNumber from "bignumber.js";
+import scientificToDecimal from "scientific-to-decimal";
+
 export const approve = async (
   busdContract,
   presaleLemaVault,
   account,
   amount
 ) => {
-  const value = amount + "000000000000000000";
+  const value = scientificToDecimal(
+    new BigNumber(amount).times(new BigNumber(10).pow(18))
+  );
   return busdContract.methods
     .approve(presaleLemaVault.options.address, value)
     .send({ from: account });
@@ -15,7 +20,9 @@ export const buyTokensWithBUSD = async (
   amount,
   account
 ) => {
-  const value = amount + "000000000000000000";
+  const value = scientificToDecimal(
+    new BigNumber(amount).times(new BigNumber(10).pow(18))
+  );
   return persaleLemaContract.methods
     .buyTokensWithBUSD(value)
     .send({ from: account });
